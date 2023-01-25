bd1715ee08b3462a0d549dd57926c5940432c5b1
Igor Kuznetsov <ik1@bk.ru>
2023-01-25 14:12:44 +0700

    рабочий веб сервер, глючит остановка

--- test json.py
+++ test json.py
@@ -1,5 +1,15 @@
 import json
-
+def allfarminfo():
+    js=list()
+    for k in farms.farms:
+        js.append(js["id"]["connection"]="3")
+        js["id"]["connection"]="3"
+       #for v in farms.get(k).Value:
+        #    js["id"]["values"][farms.get(k).getTagByShort(v).name]=farms.get(k).getValueShort(v)
+                
+    return json.dumps(js , indent=2)
 #j=json.loads('{"backendUrl":"http://admin.ifarmproject.com:3000","opcEndpoint":{"host":"10.10.13.30","port":4840,"path":"/","security":{"type":"NONE","policy":"http://opcfoundation.org/UA/SecurityPolicy#None","enabled":true,"userName":"admin","password":"wago1"}},"timeout":60000,"sessionTimeout":3600000,"logFilename":"Berdsk.log","maxBatchSize":100}')
 j=json.loads("{\"backendUrl\":\"http://admin.ifarmproject.com:3000\",\"opcEndpoint\":{\"host\":\"10.10.13.30\",\"port\":4840,\"path\":\"/\",\"security\":{\"type\":\"NONE\",\"policy\":\"http://opcfoundation.org/UA/SecurityPolicy#None\",\"enabled\":true,\"userName\":\"admin\",\"password\":\"wago1\"}},\"timeout\":60000,\"sessionTimeout\":3600000,\"logFilename\":\"Berdsk.log\",\"maxBatchSize\":100}")
-print(type(j))
+print(json.dumps(j, indent=2))
+config = uvicorn.Config("test_pgsql:app", port=8000, log_level="info")
+server = uvicorn.Server(config) 
